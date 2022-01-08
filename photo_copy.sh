5278ae53529eae3c3b1b5fa2c33d60150705be98
YAMADA Junji <junji@junyx.net>
2022-01-08 15:09:36 +0900

    JPEG, RAW, Videoファイル毎の処理を関数にまとめる (#2)

--- photo_copy.sh
+++ photo_copy.sh
@@ -11,7 +11,7 @@ CONF_FILE="${SCRIPT_DIR}/photo_copy.conf"
 if [ -f $CONF_FILE ] ; then
   source $CONF_FILE
 else
-  echo "[`date +'%Y-%m-%d %H:%M:%S'`] ERROR: File not found: ${CONF_FILE}" 1>&2
+  echo "ERROR: Configuration File not found: ${CONF_FILE}" 1>&2
   exit 1
 fi
 
@@ -24,50 +24,38 @@ if [ ! -d "$ORIGIN" ] ; then
   exit 1
 fi
 
-for date in `ls -lTtr $ORIGIN | grep rw | awk '{printf("%02d-%02d-%02d\n", $9, $6, $7);}' | sort | uniq` ; do
-  jpeg_date_dir=$JPEG_DIR/${date}
-  raw_date_dir=$RAW_DIR/${date}
-  video_date_dir=$VIDEO_DIR/${date}
+function file_copy {
+  for file in "${TARGET_FILES[@]}" ; do
+    if [ ! -d "$TARGET_DIR" ] ; then
+      mkdir -p "$TARGET_DIR"
+    fi
+    orig_path=$file
+    filename=`echo $orig_path | awk '{split($1, ary, "\/"); print ary[length(ary)]}'`
+    dest_path=$TARGET_DIR/$filename
+    echo "Copying $orig_path to $dest_path"
+    cp "$orig_path" "$dest_path"
+  done
+}
 
+for date in `ls -lTtr $ORIGIN | grep rw | awk '{printf("%02d-%02d-%02d\n", $9, $6, $7);}' | sort | uniq` ; do
   year=`echo $date | awk '{split($1, ary, "-"); printf("%d",ary[1])}'`
   month=`echo $date | awk '{split($1, ary, "-"); printf("%d",ary[2])}'`
   day=`echo $date | awk '{split($1, ary, "-"); printf("%d",ary[3])}'`
 
-  JPEG_FILES=(`ls -lTtr $ORIGIN/*{JPG,JPEG,jpg,jpeg} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
-  for file in "${JPEG_FILES[@]}" ; do
-    if [ ! -d "$jpeg_date_dir" ] ; then
-      mkdir -p "$jpeg_date_dir"
-    fi
-    orig_path=$file
-    filename=`echo $orig_path | awk '{split($1, ary, "\/"); print ary[length(ary)]}'`
-    dest_path=$jpeg_date_dir/$filename
-    echo "Copying $orig_path to $dest_path"
-    cp "$orig_path" "$dest_path"
-  done
+  # JPEG Files
+  TARGET_FILES=(`ls -lTtr $ORIGIN/*{JPG,JPEG,jpg,jpeg} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
+  TARGET_DIR=$JPEG_DIR/${date}
+  file_copy
 
-  RAW_FILES=(`ls -lTtr $ORIGIN/*{ORF,CR2,CR3} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
-  for file in "${RAW_FILES[@]}" ; do
-    if [ ! -d "$raw_date_dir" ] ; then
-      mkdir -p "$raw_date_dir"
-    fi
-    orig_path=$file
-    filename=`echo $orig_path | awk '{split($1, ary, "\/"); print ary[length(ary)]}'`
-    dest_path=$raw_date_dir/$filename
-    echo "Copying $orig_path to $dest_path"
-    cp "$orig_path" "$dest_path"
-  done
+  # RAW Files
+  TARGET_FILES=(`ls -lTtr $ORIGIN/*{ORF,CR2,CR3} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
+  TARGET_DIR=$RAW_DIR/${date}
+  file_copy
 
-  VIDEO_FILES=(`ls -lTtr $ORIGIN/*{MOV,MP4} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
-  for file in "${VIDEO_FILES[@]}" ; do
-    if [ ! -d "$video_date_dir" ] ; then
-      mkdir -p "$video_date_dir"
-    fi
-    orig_path=$file
-    filename=`echo $orig_path | awk '{split($1, ary, "\/"); print ary[length(ary)]}'`
-    dest_path=$video_date_dir/$filename
-    echo "Copying $orig_path to $dest_path"
-    cp "$orig_path" "$dest_path"
-  done
+  # Video Files
+  TARGET_FILES=(`ls -lTtr $ORIGIN/*{MOV,MP4} 2> /dev/null | awk '{print $6 " " $7 " " $9 " " $10}' | grep "$month $day $year" | awk '{print $4}'`)
+  TARGET_DIR=$VIDEO_DIR/${date}
+  file_copy
 done
 
 echo "Image file copying finished."
